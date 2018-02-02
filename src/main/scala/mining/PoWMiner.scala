52e4b1652bf89a3cadab595cdae14adfa88edbb9
catena <catena@protonmail.com>
2018-02-02 19:27:38 +0300

    week1 solution

--- src/main/scala/mining/PoWMiner.scala
+++ src/main/scala/mining/PoWMiner.scala
@@ -2,13 +2,22 @@ package mining
 
 import scorex.crypto.hash.CryptographicHash32
 
+import scala.annotation.tailrec
 import scala.math.BigInt
 
 class PoWMiner[HF <: CryptographicHash32](hashFunction: HF) {
 
   private val MaxTarget: BigInt = BigInt(1, Array.fill(32)((-1).toByte))
 
-  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = ???
+  def doWork(data: Array[Byte], difficulty: BigInt): ProvedData = {
+    @tailrec
+    def loop(nonce: Int): ProvedData = {
+      val provedData = ProvedData(data, nonce)
+      if(validateWork(provedData, difficulty)) provedData
+      else loop(nonce + 1)
+    }
+    loop(0)
+  }
 
   def validateWork(data: ProvedData, difficulty: BigInt): Boolean = realDifficulty(data) >= difficulty
 
